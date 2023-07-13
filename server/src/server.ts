0954fca4c1c042a4a7fa1a3eb7f6b07f61f8cd23
caiden20000 <121005054+caiden20000@users.noreply.github.com>
2023-07-13 03:22:51 +0000

    Install @types/express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000
 
 app.get('/', (req: any, res: any) => {
-  res.send('Hello World!')
+  res.send('Hello World! v2')
 })
 
 app.listen(port, () => {
